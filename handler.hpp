29f42592a901df012de97699560b32bcf21a3761
Kevin Calloway <kevincalloway@Kevins-MacBook-Pro.local>
2016-04-08 12:53:59 -0700

    Create directory structure for geojson files

--- handler.hpp
+++ handler.hpp
@@ -8,6 +8,7 @@
 #include "rapidjson/stringbuffer.h"
 #include <cmath>
 #include <fstream>
+#include <sys/stat.h>
 
 using namespace rapidjson;
 using namespace std;
@@ -39,20 +40,35 @@ class Handler : public osmium::handler::Handler {
 
     string quadKeyStringForLocation(const osmium::Location location, const uint z) {
       auto tileFraction = this->tileFractionFromLocation(location, z);
-      return to_string(tileFraction.x) + "/" + to_string(tileFraction.y) + "/" + to_string(tileFraction.z);
+      return to_string(tileFraction.x) + "" + to_string(tileFraction.y) + "/" + to_string(tileFraction.z);
     }
 
-    void commitToFile(const string &fileName, const StringBuffer &nodeBuffer) {
-      ofstream outfile;
-      outfile.open("test.dat", ios::out | ios::app);
-      outfile << nodeBuffer.GetString();
+    int makeDirectoryForTileFraction(const TileFraction &tileFraction) {
+      string curPath = "output";
+      mkdir( curPath.c_str(), 0777); 
+
+      curPath += "/" +  to_string(tileFraction.x);
+      mkdir( curPath.c_str(), 0777); 
+      
+      curPath += "/" +  to_string(tileFraction.y);
+      mkdir( curPath.c_str(), 0777); 
+
+      return true;
     }
 
-    void node(const osmium::Node& node) {
+    void commitToFile(const TileFraction &tileFraction, const StringBuffer &nodeBuffer) {
+      string filename = to_string(tileFraction.z);
+      string path = to_string(tileFraction.x) + "/" + to_string(tileFraction.y) + "/";
+      cout << path + "\n";
+      // ofstream outfile;
+      // outfile.open("nodes.json", ios::out | ios::app);
+      // outfile << nodeBuffer.GetString();
+    }
+
+    StringBuffer stringBufferForNode(const osmium::Node& node, const int z = 1) {
       auto const& tags = node.tags();
       double lon = node.location().lon();
       double lat = node.location().lat();
-
 
       StringBuffer nodeBuffer;
       Writer<StringBuffer> nodeWriter(nodeBuffer);
@@ -80,11 +96,13 @@ class Handler : public osmium::handler::Handler {
       nodeWriter.EndArray();
       nodeWriter.EndObject();
       nodeWriter.EndObject();
+      return nodeBuffer;
+    }
 
-      auto quadKey = this->quadKeyStringForLocation(node.location(), 1);
-
-      this->commitToFile(quadKey, nodeBuffer);
-
-      //cout << nodeBuffer.GetString()  << endl;
+    void node(const osmium::Node& node, const int z = 1) {
+      StringBuffer nodeBuffer = this->stringBufferForNode(node, z);
+      auto tileFraction = this->tileFractionFromLocation(node.location(), z);
+      this->makeDirectoryForTileFraction(tileFraction);
+      this->commitToFile(tileFraction, nodeBuffer);
     }
 };
