a17988efc805f394128c79fa0c67da6e6fa0bfca
morganherlocker <morgan@mapbox.com>
2016-04-08 11:32:30 -0700

    implement arg parsing - refs #10

--- handler.hpp
+++ handler.hpp
@@ -11,107 +11,117 @@ using namespace rapidjson;
 using namespace std;
 
 class Handler : public osmium::handler::Handler {
+  bool geojson;
+  bool pbf;
+
   public:
-    Handler() {}
+    Handler(bool createGeojson, bool createPbf) {
+      geojson = createGeojson;
+      pbf = createPbf;
+    }
 
     void node(const osmium::Node& node) {
       auto const& tags = node.tags();
       double lon = node.location().lon();
       double lat = node.location().lat();
 
-      StringBuffer nodeBuffer;
-      Writer<StringBuffer> nodeWriter(nodeBuffer);
+      if(geojson) {
+        StringBuffer nodeBuffer;
+        Writer<StringBuffer> nodeWriter(nodeBuffer);
 
-      nodeWriter.StartObject();
-      nodeWriter.Key("type");
-      nodeWriter.String("Feature");
-      nodeWriter.Key("properties");
-      nodeWriter.StartObject();
-      nodeWriter.Key("id");
-      nodeWriter.String(to_string(node.id()));
-      for (auto& tag : tags) {
-        nodeWriter.Key(tag.key());
-        nodeWriter.String(tag.value());
+        nodeWriter.StartObject();
+        nodeWriter.Key("type");
+        nodeWriter.String("Feature");
+        nodeWriter.Key("properties");
+        nodeWriter.StartObject();
+        nodeWriter.Key("id");
+        nodeWriter.String(to_string(node.id()));
+        for (auto& tag : tags) {
+          nodeWriter.Key(tag.key());
+          nodeWriter.String(tag.value());
+        }
+        nodeWriter.EndObject();
+        nodeWriter.Key("geometry");
+        nodeWriter.StartObject();
+        nodeWriter.Key("type");
+        nodeWriter.String("Point");
+        nodeWriter.Key("coordinates");
+        nodeWriter.StartArray();
+        nodeWriter.Double(lon);
+        nodeWriter.Double(lat);
+        nodeWriter.EndArray();
+        nodeWriter.EndObject();
+        nodeWriter.EndObject();
+
+        cout << nodeBuffer.GetString()  << endl;
       }
-      nodeWriter.EndObject();
-      nodeWriter.Key("geometry");
-      nodeWriter.StartObject();
-      nodeWriter.Key("type");
-      nodeWriter.String("Point");
-      nodeWriter.Key("coordinates");
-      nodeWriter.StartArray();
-      nodeWriter.Double(lon);
-      nodeWriter.Double(lat);
-      nodeWriter.EndArray();
-      nodeWriter.EndObject();
-      nodeWriter.EndObject();
-
-      //cout << nodeBuffer.GetString()  << endl;
     }
 
     void way(osmium::Way& way) {
-      auto const& tags = way.tags();
+      if(geojson) {
+        auto const& tags = way.tags();
 
-      bool polygon;
-      const char * area = tags.get_value_by_key("area");
-      const char * highway = tags.get_value_by_key("highway");
-      const char * building = tags.get_value_by_key("building");
+        bool polygon;
+        const char * area = tags.get_value_by_key("area");
+        const char * highway = tags.get_value_by_key("highway");
+        const char * building = tags.get_value_by_key("building");
 
-      // is way a polygon or a linestring?
-      if(area && strcmp(area, "yes") == 1) polygon = true;
-      else if (area && strcmp(area, "yes") == 0) polygon = false;
-      else if(building) polygon = true;
-      else if(highway) polygon = false;
-      else if (way.is_closed()) polygon = true;
-      else polygon = false;
+        // is way a polygon or a linestring?
+        if(area && strcmp(area, "yes") == 1) polygon = true;
+        else if (area && strcmp(area, "yes") == 0) polygon = false;
+        else if(building) polygon = true;
+        else if(highway) polygon = false;
+        else if (way.is_closed()) polygon = true;
+        else polygon = false;
 
-      StringBuffer wayBuffer;
-      Writer<StringBuffer> wayWriter(wayBuffer);
+        StringBuffer wayBuffer;
+        Writer<StringBuffer> wayWriter(wayBuffer);
 
-      wayWriter.StartObject();
-      wayWriter.Key("type");
-      wayWriter.String("Feature");
-      wayWriter.Key("properties");
-      wayWriter.StartObject();
-      wayWriter.Key("id");
-      wayWriter.String(to_string(way.id()));
-      for (auto& tag : tags) {
-        wayWriter.Key(tag.key());
-        wayWriter.String(tag.value());
-      }
-      wayWriter.EndObject();
-      wayWriter.Key("geometry");
-      wayWriter.StartObject();
-      wayWriter.Key("type");
-      if(polygon) {
-        wayWriter.String("Polygon");
-      } else {
-        wayWriter.String("LineString");
-      }
-      wayWriter.Key("coordinates");
-      wayWriter.StartArray();
-      if(polygon) {
-        wayWriter.StartArray();
-      }
-      for (auto& node : way.nodes()) {
-        auto location = node.location();
-        if(location) {
-          wayWriter.StartArray();
-          wayWriter.Double(node.location().lon());
-          wayWriter.Double(node.location().lat());
-          wayWriter.EndArray();
-        } else {
-          // ignore ways that are missing nodes
-          return;
+        wayWriter.StartObject();
+        wayWriter.Key("type");
+        wayWriter.String("Feature");
+        wayWriter.Key("properties");
+        wayWriter.StartObject();
+        wayWriter.Key("id");
+        wayWriter.String(to_string(way.id()));
+        for (auto& tag : tags) {
+          wayWriter.Key(tag.key());
+          wayWriter.String(tag.value());
         }
-      }
-      if(polygon) {
+        wayWriter.EndObject();
+        wayWriter.Key("geometry");
+        wayWriter.StartObject();
+        wayWriter.Key("type");
+        if(polygon) {
+          wayWriter.String("Polygon");
+        } else {
+          wayWriter.String("LineString");
+        }
+        wayWriter.Key("coordinates");
+        wayWriter.StartArray();
+        if(polygon) {
+          wayWriter.StartArray();
+        }
+        for (auto& node : way.nodes()) {
+          auto location = node.location();
+          if(location) {
+            wayWriter.StartArray();
+            wayWriter.Double(node.location().lon());
+            wayWriter.Double(node.location().lat());
+            wayWriter.EndArray();
+          } else {
+            // ignore ways that are missing nodes
+            return;
+          }
+        }
+        if(polygon) {
+          wayWriter.EndArray();
+        }
         wayWriter.EndArray();
-      }
-      wayWriter.EndArray();
-      wayWriter.EndObject();
-      wayWriter.EndObject();
+        wayWriter.EndObject();
+        wayWriter.EndObject();
 
-      //cout << wayBuffer.GetString() << endl;
+        cout << wayBuffer.GetString() << endl;
+      }
     }
 };
--- osm-split.cpp
+++ osm-split.cpp
@@ -9,7 +9,12 @@
 #include <osmium/index/map/dummy.hpp>
 #include <osmium/index/map/sparse_file_array.hpp>
 #include <osmium/handler/node_locations_for_ways.hpp>
+#include <boost/program_options.hpp>
 #include "handler.hpp"
+
+#define VERSION "v1.0.0\n"
+
+using namespace boost::program_options;
 
 typedef osmium::index::map::Dummy<osmium::unsigned_object_id_type, osmium::Location> index_neg_type;
 typedef osmium::index::map::SparseFileArray<osmium::unsigned_object_id_type, osmium::Location> index_pos_type;
@@ -19,30 +24,50 @@ int main(int argc, char** argv) {
   char cacheFile[] = "node-cache";
 
   try {
-    std::string filename = argv[1];
+    options_description desc("usage:  osm-split [options] <file>");
 
-    osmium::io::Reader reader(
-      filename,
-      osmium::osm_entity_bits::relation | osmium::osm_entity_bits::node | osmium::osm_entity_bits::way
-    );
+    desc.add_options()
+    ("help,h", "")
+    ("version,v", "")
+    ("zoom,z", "zoom level of tiles")
+    ("pbf,p", "output tiled osm.pbf files")
+    ("geojson,g", "output tiled line-delimitted geojson files");
 
-    int fd = open(cacheFile, O_RDWR);
-    if (fd == -1) {
-      std::cerr << "Can not open node cache file '" << cacheFile << "': " << strerror(errno) << "\n";
-      return 1;
+    variables_map vm;
+    store(parse_command_line(argc, argv, desc), vm);
+
+    if(vm.count("help")) {
+      cout << desc;
+    }if(vm.count("version")) {
+      cout << VERSION;
+    } else {
+      bool geojson = false;
+      bool pbf = false;
+      if(vm.count("geojson")) geojson = true;
+      if(vm.count("pbf")) geojson = pbf;
+      std::string filename = argv[1];
+
+      osmium::io::Reader reader(
+        filename,
+        osmium::osm_entity_bits::relation | osmium::osm_entity_bits::node | osmium::osm_entity_bits::way
+      );
+
+      int fd = open(cacheFile, O_RDWR);
+      if (fd == -1) {
+        std::cerr << "Can not open node cache file '" << cacheFile << "': " << strerror(errno) << "\n";
+        return 1;
+      }
+      index_pos_type index_pos {fd};
+      index_neg_type index_neg;
+      location_handler_type location_handler(index_pos, index_neg);
+      location_handler.ignore_errors();
+
+      Handler handler(geojson, pbf);
+      osmium::apply(reader, location_handler, handler);
+      reader.close();
+
+      return 0;
     }
-    index_pos_type index_pos {fd};
-    index_neg_type index_neg;
-    location_handler_type location_handler(index_pos, index_neg);
-    location_handler.ignore_errors();
-
-    Handler handler;
-    osmium::apply(reader, location_handler, handler);
-    reader.close();
-
-    std::cout << "Complete" << endl;
-
-    return 0;
   } catch (std::exception const& ex) {
     std::clog << "error: " << ex.what() << "\n";
     return -1;
